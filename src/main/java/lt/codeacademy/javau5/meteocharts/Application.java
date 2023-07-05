628d141e863d1b1671bbddc8eb52c17e2b52b0fc
Vaidas <mano@mailas.lt>
2023-07-05 08:25:19 +0300

    first commit

---
+++ src/main/java/lt/codeacademy/javau5/meteocharts/Application.java
@@ -0,0 +1,13 @@
+package lt.codeacademy.javau5.meteocharts;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Application.class, args);
+	}
+
+}
---
+++ src/main/java/lt/codeacademy/javau5/meteocharts/controllers/ApiController.java
@@ -0,0 +1,5 @@
+package lt.codeacademy.javau5.meteocharts.controllers;
+
+public class ApiController {
+
+}
