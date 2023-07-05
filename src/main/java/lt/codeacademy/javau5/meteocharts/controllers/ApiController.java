86c20dd3de5ca1ecc70bd4e5c35da1cde794c2f8
Vaidas <mano@mailas.lt>
2023-07-05 08:45:43 +0300

    Minor changes

--- src/main/java/lt/codeacademy/javau5/meteocharts/controllers/ApiController.java
+++ src/main/java/lt/codeacademy/javau5/meteocharts/controllers/ApiController.java
@@ -6,7 +6,7 @@ import java.util.List;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
+
 
 @Controller
 //@RequestMapping("/test")  //localhost:8080/test/orai
