b71c1b225bf653f6dd878d61b0b2817aa12a4d34
Vaidas <mano@mailas.lt>
2023-07-05 08:38:51 +0300

    Added chart js

--- src/main/java/lt/codeacademy/javau5/meteocharts/controllers/ApiController.java
+++ src/main/java/lt/codeacademy/javau5/meteocharts/controllers/ApiController.java
@@ -6,8 +6,10 @@ import java.util.List;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
+//@RequestMapping("/test")  //localhost:8080/test/orai
 public class ApiController {
 
 	@GetMapping("/orai")
@@ -19,6 +21,6 @@ public class ApiController {
         model.addAttribute("xData", xData);
         model.addAttribute("yData", yData);
         
-		return "index";
+		return "home";
 	}
 }
